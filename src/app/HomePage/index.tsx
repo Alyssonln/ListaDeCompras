3070e411d396387199987fcf458c521389c37265
Alyssonln <alyssonlnunes@gmail.com>
2026-07-15 08:26:35 -0300

    useState do item na HomePage

--- src/app/HomePage/index.tsx
+++ src/app/HomePage/index.tsx
@@ -5,32 +5,24 @@ import { ButtonAdd } from "@/components/ButtonAdd";
 import { FilterStatus } from "@/components/FilterStatus";
 import { StatusFilter } from "@/types/StatusFilter";
 import { Item } from "@/components/Item";
+import { useState } from "react";
 
-export function HomePage() {
-
-  const STATUS_FILTER: StatusFilter[] = [StatusFilter.DONE, StatusFilter.PENDING]
+  const STATUS_FILTER: StatusFilter[] = [StatusFilter.PENDING, StatusFilter.DONE]
 
   const LISTA_EXEMPLO = [
     {id: 1, status: StatusFilter.DONE, description: 'café'},
     {id: 2, status: StatusFilter.PENDING, description: '3 Pacotes de macarrão'},
     {id: 3, status: StatusFilter.PENDING, description: 'Pão francês'},
-    {id: 4, status: StatusFilter.PENDING, description: 'Pão francês'},
-    {id: 5, status: StatusFilter.PENDING, description: 'Pão francês'},
-    {id: 6, status: StatusFilter.PENDING, description: 'Pão francês'},
-    {id: 7, status: StatusFilter.PENDING, description: 'Pão francês'},
-    {id: 8, status: StatusFilter.PENDING, description: 'Pão francês'},
-    {id: 9, status: StatusFilter.PENDING, description: 'Pão francês'},
-    {id: 10, status: StatusFilter.PENDING, description: 'Pão francês'},
-    {id: 11, status: StatusFilter.PENDING, description: 'Pão francês'},
-    {id: 12, status: StatusFilter.PENDING, description: 'Pão francês'},
   ]
+
+export function HomePage() {
+
+  const [filter, setFilter] = useState<StatusFilter>(StatusFilter.PENDING)
 
   return (
     <View style={styles.container}>
       
       <Image source={require('@/assets/logo.png')} style={styles.imgLogo} />
-
-      
 
       <View style={styles.inputArea}>
         <InputArea 
@@ -50,7 +42,8 @@ export function HomePage() {
             <FilterStatus 
               key={status}
               status={ status }
-              isActive
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
             />
           ))
         }
@@ -79,4 +72,3 @@ export function HomePage() {
     </View>
   )
 }
-
